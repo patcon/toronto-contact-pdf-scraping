4bab0a60066460701bdac473c342d21da84659dc
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-03-31 00:30:41 -0400

    Accommodate more PDFs.

--- scrape.py
+++ scrape.py
@@ -1,30 +1,47 @@
+#!/usr/bin/env python
+import subprocess
+import sys
 import xml.etree.ElementTree as ET
+
 from xml.etree.ElementTree import tostring
 from lxml import etree
 
-tree = etree.parse('sources/city_clerk_office.xml')
-root = tree.getroot()
+try:
+    xml_file = sys.argv[1]
+except IndexError:
+    print('Usage: {} <pdf file>'.format(sys.argv[0]))
+    exit(0)
+
+proc = subprocess.Popen(["pdftohtml", xml_file,  "-xml", "-i", "-stdout"], stdout=subprocess.PIPE)
+out = proc.communicate()[0]
+
+root = etree.fromstring(out)
 
 headers = []
-headers.append(root.xpath("//fontspec[@family='Times' and @size='19' and @color='#ffffff']")[0])
-headers.append(root.xpath("//fontspec[@family='Times' and @size='16' and @color='#000000']")[0])
-headers.append(root.xpath("//fontspec[@family='Times' and @size='12' and @color='#000000']")[1])
+data = [x.split() for x in ['19 ffffff 0', '16 000000 1', '12 000000 1']]
+for datum in data:
+    size, color, i = datum
+    i = int(i)
+    q = "//fontspec[@family='Times' and @size='{}' and @color='#{}']".format(size, color)
+    fontspec = root.xpath(q)
+    headers += fontspec
 for font in headers:
     print(font.attrib)
-#elems = root.xpath(".//text[@font='9']")
-#elems = root.xpath(".//text[@font='1']")
+
 xpath_filter = ' or '.join(["@font='{}'".format(font.attrib['id']) for font in headers])
 elems = root.xpath(".//text[{}]".format(xpath_filter))
-#elems = root.xpath(".//text[" + attrib_selector + "]")
 elems = iter(elems)
 for e in elems:
-    header = e.xpath('./b')[0].text
-    if not header.strip():
+    if not e.xpath('./b'):
+        continue
+    header = e.xpath('./b')[0].text.strip()
+    if not header:
         continue
 
     next_e = e.xpath('./following-sibling::text')[0]
     if next_e.attrib['font'] == e.attrib['font']:
-        next_header = next_e.xpath('./b')[0].text
-        header = header + next_header
+        next_header = next_e.xpath('./b')[0].text.strip()
+        if next_header.startswith('Wards'): continue
+        header = header + ' ' + next_header
         elems.__next__()
-    print('"{}",{}'.format(header.strip(), e.attrib['font']))
+    print('"{}",{}'.format(header, e.attrib['font']))
